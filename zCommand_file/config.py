b818eb480941af3706cb186fc6ae30b256532829
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2021-08-18 15:30:22 +0530

    Update config.py

--- zCommand_file/config.py
+++ zCommand_file/config.py
@@ -3,12 +3,12 @@ import json,os
 path: str = os.path.join(os.getcwd(), "plugins")
 
 def json_open():
-	file = f"{path}/more_command/input.json"
+	file = f"{path}/zCommand/input.json"
 	with open(file,"r") as f:
 		n = json.load(f)
 	return n
 	
 def json_dump(n:str=None):
-	file = Path(f"plugins/more_command/input.json")
+	file = Path(f"plugins/zCommand/input.json")
 	with open(file,"w") as f:
 		json.dump(n,f)
